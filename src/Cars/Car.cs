de76b16452d673992dc2c8b41e46806550011561
Richard Orchard <me@richardorchard.com>
2020-07-22 12:17:01 +0100

    list all cars + added other base projects

--- src/Cars/Car.cs
+++ src/Cars/Car.cs
@@ -16,5 +16,30 @@ namespace Cars
         public int City { get; set; }
         public int Highway { get; set; }
         public int Combined { get; set; }
+
+       
+
+        internal static Car TransformLineToCar(string line)
+        {
+
+            var columns = line.Split(',');
+
+            return new Car
+            {
+                Year = int.Parse(columns[0]),
+                Manufacturer = columns[1],
+                Name = columns[2],
+                Displacement = double.Parse(columns[3]),
+                Cylinders = int.Parse(columns[4]),
+                City = int.Parse(columns[4]),
+                Highway = int.Parse(columns[5]),
+                Combined = int.Parse(columns[6])
+
+
+            };
+
+
+
+        }
     }
 }
--- src/Cars/Program.cs
+++ src/Cars/Program.cs
@@ -11,7 +11,26 @@ namespace Cars
     {
         static void Main(string[] args)
         {
-           
-        }        
+            var cars = ProcessFile("fuel.csv");
+
+            foreach(var car in cars)
+            {
+                Console.WriteLine(car.Name);
+
+            }
+
+        }
+
+        private static List<Car> ProcessFile(string path)
+        {
+
+            return File.ReadAllLines(path)
+                  .Skip(1)
+                  .Where(line => line.Length > 1)
+                  .Select(Car.TransformLineToCar)
+                  .ToList();
+
+        }
+
     }
 }
